8e91da7b4e4c0b90003af76a13e0ae71e94ecb5d
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-19 05:45:10 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,9 +3,9 @@ module SongsHelper
   def display_artist(song)
     if song
       song.artist_name
-      "<%= link_to song.artist_name, artist_path(song.artist) %>"
+      link_to song.artist_name, artist_path(song.artist)
     else
-      "<%= link_to 'Add Artist', edit_song_path(song) %>"
+      link_to 'Add Artist', edit_song_path(song)
     end
   end
 
